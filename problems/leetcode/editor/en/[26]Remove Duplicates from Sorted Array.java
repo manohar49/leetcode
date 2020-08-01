d409f20124385fa50e28b99f2c7e33e11d2aea50
mamano <maddineni.manohar@medlife.com>
2020-08-02 01:07:35 +0530

    removeElement

--- problems/leetcode/editor/en/[26]Remove Duplicates from Sorted Array.java
+++ problems/leetcode/editor/en/[26]Remove Duplicates from Sorted Array.java
@@ -48,7 +48,7 @@
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+class Solution26 {
     public int removeDuplicates(int[] nums) {
 
         if (nums.length == 0){
