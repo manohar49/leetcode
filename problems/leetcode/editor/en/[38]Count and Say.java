91b6d9fd61019e5ecc2f6c31d747f70315e420e2
mamano <maddineni.manohar@medlife.com>
2020-08-02 23:29:32 +0530

    countAndSay

---
+++ problems/leetcode/editor/en/[38]Count and Say.java
@@ -0,0 +1,75 @@
+// The count-and-say sequence is the sequence of integers with the first five ter
+// ms as following:
+//
+//
+// 1.     1
+// 2.     11
+// 3.     21
+// 4.     1211
+// 5.     111221
+//
+//
+// 1 is read off as "one 1" or 11.
+// 11 is read off as "two 1s" or 21.
+// 21 is read off as "one 2, then one 1" or 1211.
+//
+// Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-s
+// ay sequence. You can do so recursively, in other words from the previous member
+// read off the digits, counting the number of digits in groups of the same digit.
+//
+//
+// Note: Each term of the sequence of integers will be represented as a string.
+//
+//
+//
+//
+// Example 1:
+//
+//
+// Input: 1
+// Output: "1"
+// Explanation: This is the base case.
+//
+//
+// Example 2:
+//
+//
+// Input: 4
+// Output: "1211"
+// Explanation: For n = 3 the term was "21" in which we have two groups "2" and "
+// 1", "2" can be read as "12" which means frequency = 1 and value = 2, the same wa
+// y "1" is read as "11", so the answer is the concatenation of "12" and "11" which
+// is "1211".
+//
+// Related Topics String
+// 👍 1357 👎 9881
+
+// leetcode submit region begin(Prohibit modification and deletion)
+class Solution38 {
+  public String countAndSay(int n) {
+
+    if (n <= 0) {
+      return "";
+    }
+    if (n == 1) {
+      return "1";
+    }
+    String ret = "1";
+    while (n > 1) {
+      StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < ret.length(); i++) {
+        char c = ret.charAt(i);
+        int count = 1;
+        while (i < ret.length() - 1 && c == ret.charAt(i + 1)) {
+          count++;
+          i++;
+        }
+        sb.append(count).append(c);
+      }
+      ret = sb.toString();
+      n--;
+    }
+    return ret;
+  }
+}
+// leetcode submit region end(Prohibit modification and deletion)
